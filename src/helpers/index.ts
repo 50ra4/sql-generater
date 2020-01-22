7f66857e8aad13823db37f68ebdeedb925cf10fb
S.Igarashi <50ra4.dev.dev@gmail.com>
2020-01-22 22:03:51 +0900

    FIX: use sqlstring escape

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,10 +1,11 @@
 import { TDataType, TTargetColumn } from '../types';
+import { escape } from 'sqlstring';
 
-export const isWrapValue = (v: TDataType): boolean => typeof v === 'string' && v !== 'NULL';
+export const isNullValue = (v: TDataType): boolean => v !== 'NULL';
 
 export const wrapBracket = (v: string): string => `(${v})`;
 
-export const wrapSingleQuotation = (v: TDataType): string => (isWrapValue(v) ? `'${v}'` : `${v}`);
+export const wrapSingleQuotation = (v: TDataType): string => (isNullValue(v) ? escape(v) : `${v}`);
 
 export const toTargetColumn = (v: string | TTargetColumn): TTargetColumn =>
   typeof v !== 'string' ? v : { columnName: v };
